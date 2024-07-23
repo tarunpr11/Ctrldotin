441188a4bafd4787c027fd2162465c95ef5494bf
Tarun Parthiban <tarunpr1103@gmail.com>
2024-07-23 19:04:58 +0530

    Initial commit

---
+++ src/pages/Background.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Background = () => {
+  return (
+    <div className='iframe-container'>
+        <iframe src='https://my.spline.design/lostorbinthemountains-dc545f2e6edc04de1d838941545ea6f2/' frameborder='0' width='100%' height='100%'></iframe>
+    </div>
+  )
+}
+
+export default Background
